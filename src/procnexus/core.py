cfe9d7f300f83b2fe32feaaf3b601501ab7ebf6a
Chitaoji <2360742040@qq.com>
2026-04-11 21:14:42 +0800

    Update

--- src/procnexus/core.py
+++ src/procnexus/core.py
@@ -7,3 +7,31 @@ NOTE: this module is private. All functions and objects are available in the mai
 """
 
 __all__ = []
+
+from multiprocessing import Pool
+from typing import Callable
+
+
+def nexus[T, **P](func: Callable[P, T], processes: int = -1) -> "ProcNexus[P]":
+    if not isinstance(processes, int):
+        raise TypeError(
+            f"invalid type for processes: expected {int}, got {type(processes)} instead"
+        )
+    if not callable(func):
+        raise TypeError(f"func should be callable, got {func} instead")
+    return ProcNexus(func, processes)
+
+
+class ProcNexus[T, **P]:
+    def __init__(self, func: Callable[P, T], processes: int) -> None:
+        self.func = func
+        self.processes = processes
+        self.params: list[P] = []
+
+    def submit(self, *args: P.args, **kwargs: P.kwargs) -> None:
+        pass
+
+    def run(self) -> list[T]:
+        with Pool(processes=self.processes) as pool:
+            res = pool.starmap(self.func, self.params)
+        return res
